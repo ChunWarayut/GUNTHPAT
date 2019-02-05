87cf17b62fa71456ea439a8265761287870fcdf3
Manadsawee <mixurrnhuai@gmail.com>
2019-02-05 17:08:59 +0700

    จาก มิก
    พี่ฉุนช่วยผมด้วย

--- index.php
+++ index.php
@@ -9,17 +9,11 @@ $slide = $slide_model -> slideRoom01();
 
 ?>
 <html>
-<head>
-    <?PHP require_once('view/header.inc.php'); ?>
-</head>
-<body>
-    <?PHP require_once('view/home/index.inc.php');?>
-    <?PHP require_once('view/footer.inc.php'); ?>
-    <!-- Bootstrap core JavaScript -->
-    <script src="template/frontend/vendor/jquery/jquery.min.js"></script>
-    <script src="template/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Custom scripts for this template -->
-    <script src="template/frontend/js/agency.min.js"></script>
-</body>
-<script src="template/frontend/js/agency.min.js"></script>
+    <head>
+        <?PHP require_once('view/header.inc.php'); ?>
+    </head>
+    <body>
+        <?PHP require_once('view/home/index.inc.php');?>
+        <?PHP require_once('view/footer.inc.php'); ?>
+    </body>
 <html>
