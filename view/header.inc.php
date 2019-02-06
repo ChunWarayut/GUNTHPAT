febdf908b3ec06930fba58755124235addc2e3f6
Warayut Taekrathok <warayut2555@gmail.com>
2019-02-06 09:50:40 +0700

    Postman-win64-6.6.1-Setup

--- view/header.inc.php
+++ view/header.inc.php
@@ -3,18 +3,16 @@
 <meta name="description" content="">
 <meta name="author" content="">
 
-
 <link rel="icon" href="template/backend/images/logo/logo.png " type="image/png">
 
 <title>GUNTHPAT</title>
 <link rel="icon" href="" type="image/png">
-<script defer src="https://use.fontawesome.com/releases/v5.7.0/js/all.js"
-    integrity="sha384-qD/MNBVMm3hVYCbRTSOW130+CWeRIKbpot9/gR1BHkd7sIct4QKhT1hOPd+2hO8K" crossorigin="anonymous">
-</script>
-
+<script defer src="https://use.fontawesome.com/releases/v5.7.0/js/all.js" integrity="sha384-qD/MNBVMm3hVYCbRTSOW130+CWeRIKbpot9/gR1BHkd7sIct4QKhT1hOPd+2hO8K" crossorigin="anonymous"> </script>
 
 <!-- Bootstrap core CSS -->
 <link href="template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css"  href="template/slick/slick.css" />
 
 <!-- Custom fonts for this template -->
 <link href="template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -27,5 +25,11 @@
 <!-- Custom styles for this template -->
 <link href="template/frontend/css/menu.css" rel="stylesheet">
 
-<!-- jQuery -->
-<script src="template/vendor/jquery/jquery.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script src="template/frontend/vendor/jquery/jquery.min.js"></script>
+<script src="template/frontend/js/jquery-2.2.0.min.js" type="text/javascript"></script>
+
+<script src="template/frontend/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="template/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="template/slick/slick.min.js" type ="text/javascript" charset="utf-8"></script>
