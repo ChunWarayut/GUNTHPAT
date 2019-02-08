ed83276c18dfb2583234f53b13a7b04f9e3a1d12
chunwarayut <warayut2555@gmail.com>
2019-02-08 17:24:34 +0700

    gallery

--- view/gallery/view.inc.php
+++ view/gallery/view.inc.php
@@ -4,16 +4,16 @@ require_once('view/menu.inc.php');
 <style>
 .gal-item {
     overflow: hidden;
-    padding: 8px;
+    /* padding: 8px; */
 }
 
 .gal-item .box {
-    height: 100%;
+    /* height: 100%; */
     overflow: hidden;
 }
 
 .box img {
-    height: 100%;
+    /* height: 100%; */
     width: 100%;
     object-fit: cover;
 }
@@ -27,40 +27,42 @@ require_once('view/menu.inc.php');
 ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 co-xs-12 gal-item">
-            <div class="row h-200">
+            <div class="row ">
                 <!-- ----------------------------------------------------------------------------------- -->
-                <div class="col-md-4 col-sm-4 co-xs-12 gal-item">
-                    <div class="row">
-                        <?PHP if (!$gallery[$i]) {}else{?>
-                        <div class="col-md-12 col-sm-12 co-xs-12 gal-item">
-                            <div class="box">
-                                <img src="<?PHP echo $path_img . $gallery[$i]['gallery_img']?>"
-                                    class="img-ht img-fluid rounded">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 co-xs-12 gal-item">
+                        <div class="row">
+                            <?PHP if (!$gallery[$i]) {}else{?>
+                            <div class="col-md-12 col-sm-12 co-xs-12 gal-item">
+                                <div class="box" style="padding-top: 8px;">
+                                    <img src="<?PHP echo $path_img . $gallery[$i]['gallery_img']?>"
+                                        class="img-ht img-fluid rounded">
+                                </div>
                             </div>
-                        </div>
-                        <?PHP }  ?>
-                        <?PHP if (!$gallery[$i+1]) {}else{?>
-                        <div class="col-md-12 col-sm-12 co-xs-12 gal-item">
-                            <div class="box">
-                                <img src="<?PHP echo $path_img . $gallery[$i+1]['gallery_img']?>"
-                                    class="img-ht img-fluid rounded">
+                            <?PHP }  ?>
+                            <?PHP if (!$gallery[$i+1]) {}else{?>
+                            <div class="col-md-12 col-sm-12 co-xs-12 gal-item">
+                                <div class="box" style="padding-top: 8px;" >
+                                    <img src="<?PHP echo $path_img . $gallery[$i+1]['gallery_img']?>"
+                                        class="img-ht img-fluid rounded">
+                                </div>
                             </div>
+                            <?PHP }  ?>
                         </div>
-                        <?PHP }  ?>
                     </div>
-                </div>
-                <?PHP if (!$gallery[$i+2]) {}else{?>
-                <div class="col-md-8 col-sm-8 co-xs-12 gal-item">
-                    <div class="box">
-                        <img src="<?PHP echo $path_img . $gallery[$i+2]['gallery_img']?>"
-                            class="img-ht img-fluid rounded">
+                    <?PHP if (!$gallery[$i+2]) {}else{?>
+                    <div class="col-md-8 col-sm-8 co-xs-12 gal-item">
+                        <div class="box" style="padding-top: 8px;">
+                            <img src="<?PHP echo $path_img . $gallery[$i+2]['gallery_img']?>"
+                                class="img-ht img-fluid rounded">
+                        </div>
                     </div>
                 </div>
                 <!-- ----------------------------------------------------------------------------------- -->
                 <?PHP }  ?>
                 <?PHP if (!$gallery[$i+3]) {}else{?>
                 <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                    <div class="box">
+                    <div class="box" style="padding-top: 8px;">
                         <img src="<?PHP echo $path_img . $gallery[$i+3]['gallery_img']?>"
                             class="img-ht img-fluid rounded">
                     </div>
@@ -68,7 +70,7 @@ require_once('view/menu.inc.php');
                 <?PHP }  ?>
                 <?PHP if (!$gallery[$i+4]) {}else{?>
                 <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                    <div class="box">
+                    <div class="box" style="padding-top: 8px;">
                         <img src="<?PHP echo $path_img . $gallery[$i+4]['gallery_img']?>"
                             class="img-ht img-fluid rounded">
                     </div>
@@ -76,7 +78,7 @@ require_once('view/menu.inc.php');
                 <?PHP }  ?>
                 <?PHP if (!$gallery[$i+5]) {}else{?>
                 <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                    <div class="box">
+                    <div class="box" style="padding-top: 8px;">
                         <img src="<?PHP echo $path_img . $gallery[$i+5]['gallery_img']?>"
                             class="img-ht img-fluid rounded">
                     </div>
@@ -86,7 +88,7 @@ require_once('view/menu.inc.php');
                     <div class="row">
                         <?PHP if (!$gallery[$i+6]) {}else{?>
                         <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                            <div class="box">
+                            <div class="box" style="padding-top: 8px;">
                                 <img src="<?PHP echo $path_img . $gallery[$i+6]['gallery_img']?>"
                                     class="img-ht img-fluid rounded">
                             </div>
@@ -94,7 +96,7 @@ require_once('view/menu.inc.php');
                         <?PHP }  ?>
                         <?PHP if (!$gallery[$i+7]) {}else{?>
                         <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                            <div class="box">
+                            <div class="box" style="padding-top: 8px;">
                                 <img src="<?PHP echo $path_img . $gallery[$i+7]['gallery_img']?>"
                                     class="img-ht img-fluid rounded">
                             </div>
@@ -102,7 +104,7 @@ require_once('view/menu.inc.php');
                         <?PHP }  ?>
                         <?PHP if (!$gallery[$i+8]) {}else{?>
                         <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                            <div class="box">
+                            <div class="box" style="padding-top: 8px;">
                                 <img src="<?PHP echo $path_img . $gallery[$i+8]['gallery_img']?>"
                                     class="img-ht img-fluid rounded">
                             </div>
@@ -110,7 +112,7 @@ require_once('view/menu.inc.php');
                         <?PHP }  ?>
                         <?PHP if (!$gallery[$i+9]) {}else{?>
                         <div class="col-md-6 col-sm-6 co-xs-12 gal-item">
-                            <div class="box">
+                            <div class="box" style="padding-top: 8px;">
                                 <img src="<?PHP echo $path_img . $gallery[$i+9]['gallery_img']?>"
                                     class="img-ht img-fluid rounded">
                             </div>
