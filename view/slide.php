1f622f1f96c34012922b5cd172c167a9e2266494
Manadsawee <47310448+Manadsawee@users.noreply.github.com>
2019-02-13 13:15:59 +0700

    mix
    ข้อความในสไลด์

--- view/slide.php
+++ view/slide.php
@@ -3,36 +3,50 @@
     position: absolute;
     top: 30%;
 
-    /* padding: 24px; */
-    max-width: 10%;
+    max-width: 100%;
     z-index: 999;
     margin-left: -90
 }
 </style>
-    <div class="slide-item">
-        <div class="slide-detail" style="padding-left: 200px;">
-            <div class="slide-header" style="color: #fff">
-                <span class="text" style="font-size:20px">
-                    Welcome to
+<div class="slide-item">
+    <div class="slide-detail" style="padding-left: 200px;">
+        <div class="slide-header" style="color: #fff">
+            <span class="text" style="font-size:20px">
+                Welcome to
+            </span>
+        </div>
+        <br>
+        <div class="slide-text-detail" style="color: #fff">
+            <h1>
+                <span class="text" style="font-size:80px">
+                    GUNTHPAT PLACE
+                </span>
+        </div>
+        <br>
+        <div class="slide-text-detail" style="color: #fff">
+            <div class="font-weight-light">
+                <span class="text2" style="font-size:30px">
+                    A place to experience and enjoy life
                 </span>
             </div>
-            <br>
-            <div class="slide-text-detail" style="color: #fff">
-                <h1>
-                    <span class="text" style="font-size:60px">
-                        GUNTHPAT PLACE
-                    </span>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="slide-text-detail" style="color: #fff">
+            <div class="font-weight-light">
+                <i class="far fa-play-circle"style="font-size:100px"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </div>
-            <br>
-            <div class="slide-text-detail" style="color: #fff">
-                <div class="font-weight-light">
-                    <span class="text2" style="font-size:20px">
-                        A place to experience and enjoy life
-                    </span>
-                </div>
+        </div>
+        <div class="slide-text-detail" style="color: #fff; padding-left: 130px; margin-top: -75">
+            <div class="font-weight-light">
+                <span class="text2" style="font-size:40px;">
+                    Watch Video
+                </span>
             </div>
-        </div> 
-    </div> 
+        </div>
+    </div>
+</div>
 <section class="slide" style="margin-top: -350; ">
 
     <?php for ($i = 0; $i < count($slide); ++$i) {
