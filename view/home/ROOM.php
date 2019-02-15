b21323823d63c308c4e0e3bbf0974f3b5bb8230b
Manadsawee <47310448+Manadsawee@users.noreply.github.com>
2019-02-15 11:05:29 +0700

    adfsf

--- view/home/ROOM.php
+++ view/home/ROOM.php
@@ -1,35 +1,36 @@
+<link href="template/frontend/css/home_room.css" rel="stylesheet">
 <br>
 <br>
 <?php
 for ($i = 0; $i < count($rooms); ++$i) {
     if ($i % 2 == 0) {
         ?>
-<div class="container" style="margin-top: -250;">
-    <div class="row" style="position: relative;padding-bottom: 10%">
-        <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4" >
-            <div style="bottom: 0;position: absolute;">
+<div class="container list-one">
+    <div class="row list-br">
+        <div class="col-md-4 room-text">
+            <div class="room-detail-text">
                 <h1 class="text-left">
                     <?php  echo $i + 1; ?>
                 </h1>
                 <p class="text-left">
                     |
                 </p>
-                <h3 class="room-header">
+                <h3 class="room-text-name">
                     <?php
             echo $rooms[$i]['room_name']; ?>
                 </h3>
-                <p class="text">
+                <p>
                     <?php
             echo $rooms[$i]['room_sub_title']; ?>
                 </p>
                 <div class="container">
                     <div class="row">
-                        <h4 class="text">$
+                        <h4>$
                             <?php echo $rooms[$i]['room_price']; ?> / วัน </h4>
                         &nbsp; &nbsp; &nbsp;
                         <a href="room.php?action=detail&id=<?PHP echo $rooms[$i]['room_id']; ?>">
                             <button type="button" class="btn btn-primary text-uppercase js-scroll-trigger">
-                                <span class="text">
+                                <span>
                                     ดูเพิ่มเติม
                                 </span>
                             </button>
@@ -38,8 +39,8 @@ for ($i = 0; $i < count($rooms); ++$i) {
                 </div>
             </div>
         </div>
-        <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
-            <img style=" width: 800px; height: 400px; object-fit: cover;"
+        <div class="col-md-8">
+            <img class="img-room-show"
                 src="
             <?php
            echo $pathImg.$rooms[$i]['room_img']; ?>
@@ -60,40 +61,40 @@ for ($i = 0; $i < count($rooms); ++$i) {
     } else {
         ?>
 
-<div class="container"  style="margin-top: -130; ">
-    <div class="row" style="position: relative;padding-bottom: 15%">
-        <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
-            <img style=" width: 800px; height: 400px; object-fit: cover;"
+<div class="container list-two" >
+    <div class="row list-br">
+        <div class="col-md-8 order-2 order-md-1 ">
+            <img class="img-room-show"
                 src="
             <?php
            echo $pathImg.$rooms[$i]['room_img']; ?>
         " class="img-fluid"
                 alt="">
         </div>
-        <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-            <div style="bottom: 0;position: absolute;">
+        <div class="col-md-4 order-1 order-md-2 room-text">
+            <div class="room-detail-text">
                 <h1 class="text-left">
                     <?php  echo $i + 1; ?>
                 </h1>
                 <p class="text-left;">
                     |
                 </p>
-                <h3 class="room-header">
+                <h3 class="room-text-name">
                     <?php
             echo $rooms[$i]['room_name']; ?>
                 </h3>
-                <p class="text">
+                <p>
                     <?php
             echo $rooms[$i]['room_sub_title']; ?>
                 </p>
                 <div class="container">
                     <div class="row">
-                        <h4 class="text">$
+                        <h4>$
                             <?php echo $rooms[$i]['room_price']; ?> / วัน </h4>
                         &nbsp; &nbsp; &nbsp;
                         <a href="room.php?action=detail&id=<?PHP echo $rooms[$i]['room_id']; ?>">
                             <button type="button" class="btn btn-primary text-uppercase js-scroll-trigger">
-                                <span class="text">
+                                <span>
                                     ดูเพิ่มเติม
                                 </span>
                             </button>
