6fab1f809938c47fa91b4a98d4fc3482352cbb26
Manadsawee <47310448+Manadsawee@users.noreply.github.com>
2019-02-21 16:19:48 +0700

    facilities_th/en

--- view/facilities/view.inc.php
+++ view/facilities/view.inc.php
@@ -6,7 +6,8 @@
                     FACILITI<span style="color:#f47322;">ES</span>
                 </h1>
                 <p class="facilities-text-back">
-                    <?php echo $facilities_head[0]['facilities_head_sub_title']; ?>
+                <?php if ($lng == TH) {  echo $facilities_head[0]['facilities_head_sub_title_th'];   }else { echo $facilities_head[0]['facilities_head_sub_title_en'];  }  ?>
+                
                 </p>
             </div>
             <div class="row">
@@ -22,12 +23,13 @@
                             <div style=" padding-top: 20%; padding-bottom: 20% ">
                                 <h4>
                                     <span class="text" style="color:#FFFFFF">
-                                        <?php echo    $facilities[$i]['facilities_name']; ?>
+                                    <?php if ($lng == TH) {  echo    $facilities[$i]['facilities_name_th'];   }else { echo    $facilities[$i]['facilities_name_en'];  }  ?>
                                     </span>
                                 </h4>
                                 <p>
                                     <span class="text" style="color:#FFFFFF">
-                                        <?php echo    $facilities[$i]['facilities_detail']; ?>
+                                    <?php if ($lng == TH) {  echo    $facilities[$i]['facilities_detail_th'];   }else { echo    $facilities[$i]['facilities_detail_en'];  }  ?>
+                             
                                     </span>
                                 </p>
                                 <!-- <button type="button" class="btn btn-outline-light">MORE</button> -->
