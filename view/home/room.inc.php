419d6a6eb85ac2b62d19927dabea98762d06a071
Manadsawee <47310448+Manadsawee@users.noreply.github.com>
2019-02-21 15:36:06 +0700

    mix edit:homeroom/footer/slide_th/en

--- view/home/room.inc.php
+++ view/home/room.inc.php
@@ -11,17 +11,20 @@ for ($i = 0; $i < count($rooms); ++$i) {
                 <p class="text-left">
                     |
                 </p>
-                <span class="h3 room-text-name"><?php if ($lng == TH) {  echo $rooms[$i]['room_name_th'];   }else { echo $rooms[$i]['room_name_en'];  }  ?> </span>
+                <span
+                    class="h3 room-text-name"><?php if ($lng == TH) {  echo $rooms[$i]['room_name_th'];   }else { echo $rooms[$i]['room_name_en'];  }  ?>
+                </span>
                 <div class="room-text-detail">
                     <div class="text-content-room" style="overflow-wrap: break-word;">
-                        <?php echo $rooms[$i]['room_sub_title']; ?>
+                        <?php if ($lng == TH) {  echo $rooms[$i]['room_sub_title_th'];   }else { echo $rooms[$i]['room_sub_title_en'];  }  ?>
+
                     </div>
                 </div>
                 <div class="row">
-                    <div  class="col-12 justify-content-end d-flex align-items-center">
+                    <div class="col-12 justify-content-end d-flex align-items-center">
                         <span class="h4 m-0 mr-2">$ <?php echo $rooms[$i]['room_price']; ?> / วัน </span>
                         <a class="btn btn-orange" href="room.php?action=detail&id=<?PHP echo $rooms[$i]['room_id']; ?>">
-                                    ดูเพิ่มเติม
+                            ดูเพิ่มเติม
                         </a>
                     </div>
                 </div>
@@ -57,17 +60,19 @@ for ($i = 0; $i < count($rooms); ++$i) {
                 <p class="text-left">
                     |
                 </p>
-                <span class="h3 room-text-name"><?php echo $rooms[$i]['room_name']; ?></span>
+                <span class="h3 room-text-name">
+                    <?php if ($lng == TH) {  echo $rooms[$i]['room_name_th'];   }else { echo $rooms[$i]['room_name_en'];  }  ?>
+                </span>
                 <div class="room-text-detail">
                     <div class="text-content-room" style="overflow-wrap: break-word;">
-                        <?php echo $rooms[$i]['room_sub_title']; ?>
+                    <?php if ($lng == TH) {  echo $rooms[$i]['room_sub_title_th'];   }else { echo $rooms[$i]['room_sub_title_en'];  }  ?>
                     </div>
                 </div>
                 <div class="row">
-                    <div  class="col-12 justify-content-end d-flex align-items-center">
+                    <div class="col-12 justify-content-end d-flex align-items-center">
                         <span class="h4 m-0 mr-2">$ <?php echo $rooms[$i]['room_price']; ?> / วัน </span>
                         <a class="btn btn-orange" href="room.php?action=detail&id=<?PHP echo $rooms[$i]['room_id']; ?>">
-                                    ดูเพิ่มเติม
+                            ดูเพิ่มเติม
                         </a>
                     </div>
                 </div>
