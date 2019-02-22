22b9e26bb79b907a55b6d4cc4e99dc978613bcb8
chunwarayut <warayut2555@gmail.com>
2019-02-22 10:46:42 +0700

    Update index.inc.php

--- view/gallery/index.inc.php
+++ view/gallery/index.inc.php
@@ -4,6 +4,18 @@ $path = "view/gallery/";
 $path_img = "img_upload/gallery/";
 
 
+$pathImgGallery = "img_upload/gallery/";
+require_once('models/GalleryModel.php');
+$gallery_model = new Gallery;
+$gallery = $gallery_model -> getgalleryby($_GET['id']) ;
+
+$room_id = $_POST['room_id'];
+
+if( !isset($_GET['action']) ) {
+    require_once($path.'view.inc.php');
+} else if( $_GET['action'] == "search") {
+    $gallery = $gallery_model -> getgalleryby($_POST['room_id']) ;
+}
 
 
 
