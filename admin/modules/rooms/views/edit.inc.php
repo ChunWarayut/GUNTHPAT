4bd133ef47fb36b759360be5e39c692c9a07dd18
chunwarayut <warayut2555@gmail.com>
2019-02-22 16:34:27 +0700

    Update edit.inc.php

--- admin/modules/rooms/views/edit.inc.php
+++ admin/modules/rooms/views/edit.inc.php
@@ -203,7 +203,7 @@ function readURL(input) {
                                                 <label>Size EN<font color="#F00"><b>*</b></font></label>
                                                 <input id="room_size_en" name="room_size_en" class="form-control"
                                                     value="<?PHP echo $roomBy[0]['room_size_en'];?>" />
-                                                <p class="help-block">Example : 50 ตารางเมตร </p>
+                                                <p class="help-block">Example : 50 square meters </p>
                                             </div>
                                         </div>
 
@@ -213,7 +213,7 @@ function readURL(input) {
                                                 <label>Detail 1 EN<font color="#F00"><b>*</b></font></label>
                                                 <input id="room_detail_1_en" name="room_detail_1_en" class="form-control"
                                                     value="<?PHP echo $roomBy[0]['room_detail_1_en'];?>" />
-                                                <p class="help-block">Example : ผู้ใหญ่ 2 ท่าน + เด็ก 1 ท่าน </p>
+                                                <p class="help-block">Example : 2 adults + 1 child </p>
                                             </div>
                                         </div>
 
@@ -222,7 +222,7 @@ function readURL(input) {
                                                 <label>Detail 2 EN<font color="#F00"><b>*</b></font></label>
                                                 <input id="room_detail_2_en" name="room_detail_2_en" class="form-control"
                                                     value="<?PHP echo $roomBy[0]['room_detail_2_en'];?>" />
-                                                <p class="help-block">Example : ระเบียงส่วนตัว </p>
+                                                <p class="help-block">Example : Private balcony </p>
                                             </div>
                                         </div>
 
@@ -231,7 +231,7 @@ function readURL(input) {
                                                 <label>Detail 3 EN<font color="#F00"><b>*</b></font></label>
                                                 <input id="room_detail_3_en" name="room_detail_3_en" class="form-control"
                                                     value="<?PHP echo $roomBy[0]['room_detail_3_en'];?>" />
-                                                <p class="help-block">Example : วิวห้องหรือสวน </p>
+                                                <p class="help-block">Example : Room or garden view </p>
                                             </div>
                                         </div>
 
@@ -240,7 +240,7 @@ function readURL(input) {
                                                 <label>Facility 1 EN<font color="#F00"><b>*</b></font></label>
                                                 <input id="room_facility_1_en" name="room_facility_1_en" class="form-control"
                                                     value="<?PHP echo $roomBy[0]['room_facility_1_en'];?>" />
-                                                <p class="help-block">Example : บริการโทรศัพท์ห้อง </p>
+                                                <p class="help-block">Example : Room telephone service </p>
                                             </div>
                                         </div>
 
@@ -249,7 +249,7 @@ function readURL(input) {
                                                 <label>Facility 2 EN<font color="#F00"><b>*</b></font></label>
                                                 <input id="room_facility_2_en" name="room_facility_2_en" class="form-control"
                                                     value="<?PHP echo $roomBy[0]['room_facility_2_en'];?>" />
-                                                <p class="help-block">Example : บริการ WiFi ฟรี </p>
+                                                <p class="help-block">Example : Free WiFi service </p>
                                             </div>
                                         </div>
 
