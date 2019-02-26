d9ef8ece8abe455306512da624d962c5bb7469c9
chunwarayut <warayut2555@gmail.com>
2019-02-26 10:46:53 +0700

    gallery

--- view/home/gallery.inc.php
+++ view/home/gallery.inc.php
@@ -28,13 +28,112 @@
 
 
 <div class="gallery-slide">
-
-<section class="slide-galery" >
-    <?php for ($i=0; $i < count($gallery); $i++) {    ?>
-    <img class="img-fluid" style=" width: 100px; height: 200px; object-fit: cover;"
-        src="<?PHP echo $pathImgGallery . $gallery[$i]['gallery_img']?>">
-    <?php } ?>
-</section>
+<?PHP
+// for ($i=0; $i < count($gallery); $i= $i + 10) { 
+for ($i=0; $i < 20; $i= $i + 10) { ?>
+        <div class="row">
+            <div class="col-12 col-12 co-12 gal-item">
+                <div class="row ">
+                    <!-- ----------------------------------------------------------------------------------- -->
+                    <div class="row">
+                        <div class="col-4 col-4 co-12 gal-item">
+                            <div class="row">
+                                <?PHP if (!$gallery[$i]) {}else{?>
+                                <div class="col-12 col-12 co-12 gal-item">
+                                    <div class="box">
+                                        <img src="<?PHP echo $pathImgGallery . $gallery[$i]['gallery_img']?>"
+                                            class="img-ht img-fluid rounded">
+                                    </div>
+                                </div>
+                                <?PHP }  ?>
+                                <?PHP if (!$gallery[$i+1]) {}else{?>
+                                <div class="col-12 col-12 co-12 gal-item">
+                                    <div class="box">
+                                        <img src="<?PHP echo $pathImgGallery . $gallery[$i+1]['gallery_img']?>"
+                                            class="img-ht img-fluid rounded">
+                                    </div>
+                                </div>
+                                <?PHP }  ?>
+                            </div>
+                        </div>
+                        <?PHP if (!$gallery[$i+2]) {}else{?>
+                        <div class="col-8 col-8 co-12 gal-item">
+                            <div class="box">
+                                <img src="<?PHP echo $pathImgGallery . $gallery[$i+2]['gallery_img']?>"
+                                    class="img-ht img-fluid rounded">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ----------------------------------------------------------------------------------- -->
+                    <?PHP }  ?>
+                    <div class="row">
+                        <?PHP if (!$gallery[$i+3]) {}else{?>
+                        <div class="col-6 col-6 co-12 gal-item">
+                            <div class="box">
+                                <img src="<?PHP echo $pathImgGallery . $gallery[$i+3]['gallery_img']?>"
+                                    class="img-ht img-fluid rounded">
+                            </div>
+                        </div>
+                        <?PHP }  ?>
+                        <?PHP if (!$gallery[$i+4]) {}else{?>
+                        <div class="col-6 col-6 co-12 gal-item">
+                            <div class="box">
+                                <img src="<?PHP echo $pathImgGallery . $gallery[$i+4]['gallery_img']?>"
+                                    class="img-ht img-fluid rounded">
+                            </div>
+                        </div>
+                        <?PHP }  ?>
+                        <?PHP if (!$gallery[$i+5]) {}else{?>
+                        <div class="col-6 col-6 co-12 gal-item">
+                            <div class="box">
+                                <img src="<?PHP echo $pathImgGallery . $gallery[$i+5]['gallery_img']?>"
+                                    class="img-ht img-fluid rounded">
+                            </div>
+                        </div>
+                        <?PHP }  ?>
+                        <div class="col-6 col-6 co-12 gal-item">
+                            <div class="row">
+                                <?PHP if (!$gallery[$i+6]) {}else{?>
+                                <div class="col-6 col-6 co-12 gal-item">
+                                    <div class="box">
+                                        <img src="<?PHP echo $pathImgGallery . $gallery[$i+6]['gallery_img']?>"
+                                            class="img-ht img-fluid rounded">
+                                    </div>
+                                </div>
+                                <?PHP }  ?>
+                                <?PHP if (!$gallery[$i+7]) {}else{?>
+                                <div class="col-6 col-6 co-12 gal-item">
+                                    <div class="box">
+                                        <img src="<?PHP echo $pathImgGallery . $gallery[$i+7]['gallery_img']?>"
+                                            class="img-ht img-fluid rounded">
+                                    </div>
+                                </div>
+                                <?PHP }  ?>
+                                <?PHP if (!$gallery[$i+8]) {}else{?>
+                                <div class="col-6 col-6 co-12 gal-item">
+                                    <div class="box">
+                                        <img src="<?PHP echo $pathImgGallery . $gallery[$i+8]['gallery_img']?>"
+                                            class="img-ht img-fluid rounded">
+                                    </div>
+                                </div>
+                                <?PHP }  ?>
+                                <?PHP if (!$gallery[$i+9]) {}else{?>
+                                <div class="col-6 col-6 co-12 gal-item">
+                                    <div class="box">
+                                        <img src="<?PHP echo $pathImgGallery . $gallery[$i+9]['gallery_img']?>"
+                                            class="img-ht img-fluid rounded">
+                                    </div>
+                                </div>
+                                <?PHP }  ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <?PHP } ?>
+    
 
 </div>
 
