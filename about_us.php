d9de017bd5826c7968e580447e742d400ad3a236
nattavee tantragool <devilzeros@outlook.co.th>
2019-03-07 17:50:42 +0700

    est edit : share

--- about_us.php
+++ about_us.php
@@ -19,11 +19,6 @@ $page = $page_model->getPageByID('2');
 <html>
 <head>
 
-<?php if($menu != 'room'){ ?>
-    <meta property="og:type"          content="website">
-    <meta property="og:title"         content="<?PHP if($lng == "TH"){ echo $page['page_title_th']; }else{ echo $page['page_title_en'];} ?>">
-    <meta property="og:description"   content="<?PHP if($lng == "TH"){ echo $page['page_description_th']; }else{ echo $page['page_description_en']; } ?>">
-    <?php } ?>
 
 
     <?PHP require_once('view/header.inc.php'); ?>
